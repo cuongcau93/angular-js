397b9c66e21d3d3027c4a935e2ddb52f57d3351c
cuongcau93 <cuongnm22@wru.vn>
2017-05-05 18:00:15 +0700

    sss
    sss

---
+++ APM/app/products/product-filter.pipe.ts
@@ -0,0 +1,10 @@
+import { PipeTransform } from "@angular/core";
+
+export class ProductFilterPipe implements PipeTransform {
+    
+    transform(value: any, ...args: any[]) {
+        throw new Error('Method not implemented.');
+    }
+
+
+}
